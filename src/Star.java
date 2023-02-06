95d0f12e2d8a5e9a58e1893e5a9c8afc66005301
PDarija <124299423+PDarija@users.noreply.github.com>
2023-02-06 18:11:36 +0200

    Update Star.java

--- src/Star.java
+++ src/Star.java
@@ -1,2 +1,18 @@
 package PACKAGE_NAME;public class Star {
+          System.out.println("              *             ");
+        System.out.println("             ***              ");
+        System.out.println("            *****             ");
+        System.out.println("           *******            ");
+        System.out.println("          *********           ");
+        System.out.println("         ***********          ");
+        System.out.println("***************************** ");
+        System.out.println("  ************************    ");
+        System.out.println("     *******************      ");
+        System.out.println("      *****************       ");
+        System.out.println("     *******************      ");
+        System.out.println("    *********   *********     ");
+        System.out.println("   *******         *******    ");
+        System.out.println("  *****               *****   ");
+        System.out.println(" ***                     ***  ");
+        System.out.println("*                           * ");
 }
