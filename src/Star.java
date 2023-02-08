c07b80d677dc2db8ffd7a0161b8807c93cdd78b7
Darija Paulska <paulskadarija@gmail.com>
2023-02-08 21:15:40 +0200

    Second Homework

--- src/Star.java
+++ src/Star.java
@@ -1,20 +1,2 @@
-public class Star {
-    public static void main(String[] args) {
-          System.out.println("              *             ");
-        System.out.println("             ***              ");
-        System.out.println("            *****             ");
-        System.out.println("           *******            ");
-        System.out.println("          *********           ");
-        System.out.println("         ***********          ");
-        System.out.println("***************************** ");
-        System.out.println("  ************************    ");
-        System.out.println("     *******************      ");
-        System.out.println("      *****************       ");
-        System.out.println("     *******************      ");
-        System.out.println("    *********   *********     ");
-        System.out.println("   *******         *******    ");
-        System.out.println("  *****               *****   ");
-        System.out.println(" ***                     ***  ");
-        System.out.println("*                           * ");
-}
+package PACKAGE_NAME;public class Star {
 }
