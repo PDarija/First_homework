4480d2fde5fe6c7bab4374b6a1bae114ffc032ec
Darija Paulska <paulskadarija@gmail.com>
2023-02-08 21:11:29 +0200

    Second Homework

---
+++ src/Hometask_2.java
@@ -0,0 +1,24 @@
+public class Hometask_2 {
+    public static void main(String[] args) {
+        //First Task
+        int a;
+        int b;
+        a=3;
+        b=5;
+        int sqrt =a*b;
+
+        //Second Task
+        double c;
+        double j;
+        c=55.5;
+        j=1.5;
+
+        double BMI = (double) c/(j*j) ;
+
+        System.out.println("Square root is:" +sqrt);
+        System.out.println("Body mass index: " +BMI);
+
+
+
+    }
+}
