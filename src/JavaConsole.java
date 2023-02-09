c27f0858ddcb663be5abe4788e1958c6a636bacf
Darija Paulska <paulskadarija@gmail.com>
2023-02-09 18:46:23 +0200

    Second Homework with the 3 task

---
+++ src/JavaConsole.java
@@ -0,0 +1,26 @@
+import com.sun.jdi.connect.Connector;
+
+import java.util.Scanner;
+public class JavaConsole {
+    public static void main(String args[]) {
+
+        int firstnumber;
+        int secondnumber;
+        int result;
+
+       Scanner in = new Scanner(System.in);
+
+        System.out.println("Please define two number and I will multiply it" );
+        System.out.println("First number is?");
+        firstnumber=in.nextInt();
+        System.out.println("The second number is?");
+        secondnumber=in.nextInt();7
+
+
+        result = firstnumber * secondnumber;
+        System.out.println("The result is " + result);
+
+
+    }
+
+}
